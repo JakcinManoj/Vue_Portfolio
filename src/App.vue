7c45ce1398724efa567168492cbae52ceffd5517
Jakcin <jakcinmanojjacobraj@devaxcel.com>
2023-03-05 16:13:56 +0530

    may be fixed issue testing via deploy

--- src/App.vue
+++ src/App.vue
@@ -65,7 +65,7 @@ footer {
   padding: 2em 0;
   display: grid;
   margin-top: 5%;
-  position: relative;
+  position: fixed;
   bottom: 0;
   width:100%;
 }
@@ -84,6 +84,10 @@ footer a, footer p {
   text-decoration: none;
   color: #fff;
 }
-
+@media screen and (max-width: 1245px) {
+  footer {
+    position: relative;
+  }
+}
 
 </style>
